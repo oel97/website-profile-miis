165b30dfb3291c288846c9e9fd04810d8843fbdf
Leo Prayoga Saputra <leoprayogasaputra97@gmail.com>
2026-09-03 16:56:40 +0700

    Fix HTTPS proxy on Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
             'token' => $token,
             'email' => $notifiable->getEmailForPasswordReset(),
         ]));
+        if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
     }
 }
